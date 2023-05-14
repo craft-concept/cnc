a57e2c08a8549a54c4fffc6f0e671adc27b3cff3
Jeff Peterson <jeff@yak.sh>
2023-05-14 06:35:32 -0700

    inflect cnc

--- lib/cnc/engine.rb
+++ lib/cnc/engine.rb
@@ -1,5 +1,9 @@
 module CNC
   class Engine < ::Rails::Engine
     isolate_namespace CNC
+
+    initializer 'setup_inflections' do
+      ActiveSupport::Inflector.inflections {|inflect| inflect.acronym 'CNC' }
+    end
   end
 end
